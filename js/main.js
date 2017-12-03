94a04596120eda070628ea6edbadb3affbbe0a61
jameswquinn <jameswquinn@users.noreply.github.com>
2017-12-03 23:25:18 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 Moon.component("modal", {
     template: `<div><button m-on:click="popup">Popup</button></div>`,
     methods: {
-
-        popup: function() {
-          var url = app.get("img");
+          popup: function() {
+            var change = app.set("img2", "https://i.pinimg.com/736x/83/ef/d2/83efd2f83d050642e5de3adbbfcfd5bc--black-white-fashion-fashion-portraits.jpg");
+            var url = app.get("img2");
             var popup = {
                 body: '<figure><img class="lazyload" data-src="' + url + '" /><figcaption><blockquote><p><em>Yeah!! Milligram is amazing.</em></p></blockquote><p><em>Yeah!! Milligram is amazing.</em></p></figcaption></figure>',
                 buttons: {
@@ -21,7 +21,8 @@ Moon.component("modal", {
 const app = new Moon({
     el: "#app",
     data: {
-  img: "https://www.maybelline.com/~/media/mny/us/eye-makeup/eye-shadow/the-rock-nudes-palette/modules/featuredproductwide/maybelline-eyeshadow-rocknudes-beauty-look-16x9.jpg",
-},
+        img: "https://www.maybelline.com/~/media/mny/us/eye-makeup/eye-shadow/the-rock-nudes-palette/modules/featuredproductwide/maybelline-eyeshadow-rocknudes-beauty-look-16x9.jpg",
+        img2: ""
+    },
     template: '<modal></modal>'
 });
