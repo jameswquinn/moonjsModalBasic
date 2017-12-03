2a281a4704ca90335378c733e83f09e618b2d11d
jameswquinn <jameswquinn@users.noreply.github.com>
2017-12-03 19:55:27 +0000

    update

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ Moon.component("modal", {
         popup: function() {
           var url = 'https://www.maybelline.com/~/media/mny/us/eye-makeup/eye-shadow/the-rock-nudes-palette/modules/featuredproductwide/maybelline-eyeshadow-rocknudes-beauty-look-16x9.jpg';
             var popup = {
-                body: '<figure><img src="' + url + '" /><figcaption><blockquote><p><em>Yeah!! Milligram is amazing.</em></p></blockquote><p><em>Yeah!! Milligram is amazing.</em></p></figcaption></figure>',
+                body: '<figure><img class="lazyload" data-src="' + url + '" /><figcaption><blockquote><p><em>Yeah!! Milligram is amazing.</em></p></blockquote><p><em>Yeah!! Milligram is amazing.</em></p></figcaption></figure>',
                 buttons: {
                     cancel: {
                         title: 'Dismiss',
